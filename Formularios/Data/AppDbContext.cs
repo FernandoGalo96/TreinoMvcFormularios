ecc56d384ee705af52919481b50d3d822ec50bec
Fernando Galo <fergalo.alves@gmail.com>
2024-08-12 13:55:16 -0300

    Comitando

---
+++ Formularios/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Formularios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Formularios.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Aluno> Alunos { get; set; }
+}
